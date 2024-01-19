7a8a6bc164b4fbf570136fba13e712231796fb97
Tom <tom.dardilhac@laposte.net>
2024-01-19 18:43:37 +0100

    Ameliorations

--- script.js
+++ script.js
@@ -1,46 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  var cardToggle = document.getElementById("card__toggle");
+  var btnShower = document.getElementById("btn_shower");
+  var open = false;
 
-    var cardToggle = document.getElementById('card__toggle');
-    var btnShower = document.getElementById("btn_shower");
-    var open = false;
+  function ShowMore() {
+    open = true;
+    cardToggle.classList.remove("hidden");
+    btnShower.innerHTML = "Masquer les voyages";
+  }
 
-    function ShowMore(){
-        open = true;
-        cardToggle.classList.remove('hidden');
-        btnShower.innerHTML = "Masquer les voyages"
+  function CloseDiv() {
+    open = false;
+    cardToggle.classList.add("hidden");
+    btnShower.innerHTML = "Afficher plus de voyage";
+  }
+
+  btnShower.onclick = function () {
+    if (!open) {
+      ShowMore();
+    } else {
+      CloseDiv();
     }
+  };
 
-    function CloseDiv(){
-        open = false;
-        cardToggle.classList.add('hidden');
-        btnShower.innerHTML = "Afficher plus de voyage"
-    }
-
-    btnShower.onclick = function(){
-        if(!open){
-            ShowMore();
-        }else{
-            CloseDiv();
-        }
-    }
+  
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    console.log('Le DOM est chargé. :) ');
-  });
+  console.log("Le DOM est chargé. :) ");
+});
